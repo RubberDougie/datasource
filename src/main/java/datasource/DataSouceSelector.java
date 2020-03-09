e1a8acf76681153069b61486e13c57d71d8edc3d
Douglas Raymond Siano <DouglasSiano@gmail.com>
2020-03-09 18:02:54 -0400

    Added source selector function

---
+++ src/main/java/datasource/DataSouceSelector.java
@@ -0,0 +1,34 @@
+package datasource;
+
+public class DataSouceSelector {
+	public static DataSource dataSourceSelector(String sourceType) {
+		DataSource dataSource = null;
+		switch (sourceType) {
+		case "CONSOLE INPUT": {
+			dataSource = new ConsoleInput();
+			break;
+		}
+		case "SQL SOURCE": {
+			dataSource = new SqlSource();
+			break;
+		}
+		case "MYSQL SOURCE": {
+			dataSource = new MySqlSource();
+			break;
+		}
+		case "FILE SOURCE": {
+			dataSource = new FileSource();
+			break;
+		}
+		case "DISTRIBUTED FILE SOURCE": {
+			dataSource = new DistributedFileSource();
+			break;
+		}
+		default: {
+			System.exit(1);
+		}
+		}
+
+		return dataSource;
+	}
+}
